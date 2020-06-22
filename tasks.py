97cb07af71403c07c9531fd6fff5ddd0dbda4d73
Eric Busboom <eric@civicknowledge.com>
2020-06-22 10:05:31 -0700

    .

--- tasks.py
+++ tasks.py
@@ -1,18 +1,37 @@
+# Task definitions for invoke
+# You must first install invoke, https://www.pyinvoke.org/
+
+# You can also create you own tasks
 from invoke import task
 
-s3_bucket='library.metatab.org'
-wp_site='data.sandiegodata.org'
+from metapack_build.tasks.package import ns
+
+# To configure options for invoke functions you can:
+# - Set values in the 'invoke' section of `~/.metapack.yaml
+# - Use one of the other invoke config options:
+#   http://docs.pyinvoke.org/en/stable/concepts/configuration.html#the-configuration-hierarchy
+# - Set the configuration in this file:
+
+# ns.configure(
+#    {
+#        'metapack':
+#            {
+#                's3_bucket': 'bucket_name',
+#                'wp_site': 'wp sot name',
+#                'groups': None,
+#                'tags': None
+#            }
+#    }
+# )
+
+# However, the `groups` and `tags` hould really be set in the `metatada.csv`
+# file, and `s3_bucket` and `wp_site` should be set at the collection or global level
+
 
 @task
-def build(c, force=False):
-    c.run(f"mp build -r {'-F' if force else ''}")
-   
-@task
-def s3(c):
-    c.run(f"mp s3 -s {s3_bucket}")
-   
-    
-@task
-def publish(c):
-    c.run(f"mp s3 -s {s3_bucket}")
-    c.run(f"mp wp -s {wp_site} -p")
+def example_task(c):
+    """An exmaple Invoke task"""
+    c.run("echo 'this is an example task' ")
+
+
+ns.add_task(example_task)
